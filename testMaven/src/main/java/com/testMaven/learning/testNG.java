5e4c61de764faa0200075c6abea832173ce4dd39
chenxiao <scxiaoc@163.com>
2018-10-28 17:26:42 +0800

    创建Maven学习项目

---
+++ testMaven/src/main/java/com/testMaven/learning/testNG.java
@@ -0,0 +1,16 @@
+package com.testMaven.learning;
+
+import org.testng.annotations.Test;
+
+public class testNG {
+
+    @Test
+    public void  test(){
+        int a= 4,b=2;
+        int c= a/b;
+        System.out.print("a 除 b = "+c);
+
+
+    }
+
+}
