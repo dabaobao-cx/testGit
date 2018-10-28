50976343d4cd9adce88fd27266105dba97112dcb
chenxiao <scxiaoc@163.com>
2018-10-28 18:27:40 +0800

    添加test2

---
+++ testMaven/src/main/java/com/testMaven/learning/test2.java
@@ -0,0 +1,12 @@
+package com.testMaven.learning;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+
+    @Test
+    public void test(){
+
+        System.out.println("新的一个Java文件测试!");
+    }
+}
